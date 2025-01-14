ffd390a05cc27559243072e920ba6975694eb74d
inoueyuji <sikogaku2593@gmail.com>
2025-01-14 21:49:46 +0900

    Add:hamburgermenu

--- app/javascript/dropdown-menu.js
+++ app/javascript/dropdown-menu.js
@@ -1,12 +1,21 @@
-  // Get the checkbox and dropdown menu
+  // swapボタンのtrue or false をidで取得
+  // デフォルトが'hidden'クラスを持つdropdown-menuを取得
   const menuToggle = document.getElementById('menu-toggle');
   const dropdownMenu = document.getElementById('dropdown-menu');
 
-  // Toggle dropdown menu visibility
+  // Toggle dropdown をswapボタンの変化で開閉させる処理
   menuToggle.addEventListener('change', () => {
     if (menuToggle.checked) {
       dropdownMenu.classList.remove('hidden'); // Show menu
     } else {
       dropdownMenu.classList.add('hidden'); // Hide menu
+    }
+  });
+
+  // ドロップダウンメニューの外側をクリックしたらメニューが閉じる処理
+  document.addEventListener('click', (e) => {
+    if (!menuToggle.contains(e.target) && !dropdownMenu.contains(e.target)) {
+      menuToggle.checked =false;
+      dropdownMenu.classList.add('hidden');
     }
   });
