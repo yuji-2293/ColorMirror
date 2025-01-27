c2bfc2e88b10ba572a8d46a7370dcdd6fef665b7
inoueyuji <sikogaku2593@gmail.com>
2025-01-27 23:47:19 +0900

    feature_Weather

---
+++ app/services/weather_service.rb
@@ -0,0 +1,15 @@
+class WeatherService
+  include HTTParty
+
+  base_uri "api.openweathermap.org"
+
+  def initialize(city)
+    api_key = ENV["OPNEWEATHERMAP_API_KEY"]
+    @options = { query: { q: "#{city},jp", appid: api_key, lang: "ja" } }
+  end
+
+  def fetch_weather
+    self.class.get("/data/2.5/weather", @options)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,9 @@ Rails.application.routes.draw do
   # chatbot機能
   get "chatbots/ask" => "chatbots#ask"
   post "chatbots/answer" => "chatbots#answer"
+  # OpenWeatherAPI機能のルーティング
+  get 'weather', to: 'weathers#index', as: :weather
+  get 'weather/show', to: 'weathers#show', as: :show_weather
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
