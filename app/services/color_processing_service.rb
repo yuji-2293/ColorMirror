aeb5b44d2e637ab3f080cf6d776d3c065183981c
inoueyuji <sikogaku2593@gmail.com>
2025-02-06 17:04:33 +0900

    fix:formオブジェクトの導入

--- app/services/color_processing_service.rb
+++ app/services/color_processing_service.rb
@@ -5,15 +5,16 @@ class ColorProcessingService
     @user = current_user
   end
 
-  def process_color(mapped_color)
+  def process_color(mapped_color, weather_data)
     # プロンプトの生成mapped_colorを引数にとり、AIに渡す
-    prompt = "色「#{ mapped_color }」についてコメントを生成してください。"
+    prompt = "
+              気分を表す色 =#{ mapped_color },
+              外部環境 = #{ weather_data[:weather_name] },#{ weather_data[:weather_pressure] }hpa,#{ weather_data[:temperature] }℃
+              気分を表す色, 外部環境をもとにコメントを生成してください
+              その際、冒頭で外部環境について復唱してください"
     # 別のserviceクラスからColorResponseServiceをインスタンス化、レスポンスの生成
     ai_response = ::ColorResponseService.new.fetch_response(prompt)
     # レスポンスの成形
-    color_response = ai_response["choices"][0]["message"]["content"]
-    # colorの関連テーブルとai_commentカラムへの保存
-    self_log = @color.self_logs.create(user: @user)
-    @response = self_log.responses.create(ai_comment: color_response)
+    ai_response["choices"][0]["message"]["content"]
   end
 end
