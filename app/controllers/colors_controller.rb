49fff0605fe6021c83b2cd9122a7a1ec8fe7d377
inoueyuji <sikogaku2593@gmail.com>
2025-02-06 21:09:36 +0900

    lint_check

--- app/controllers/colors_controller.rb
+++ app/controllers/colors_controller.rb
@@ -17,7 +17,7 @@ class ColorsController < ApplicationController
     mapped_color = ColorMapping.mapping_color(color_params[:color_name])
     weather_data = color_params.slice(:weather_name, :weather_pressure, :temperature)
     # AIレスポンス生成のserviceを呼び出す/必要な引数を渡す、生成したレスポンスを変数に格納
-    ai_comment = ColorProcessingService.new(@color, current_user, ).process_color(mapped_color,weather_data)
+    ai_comment = ColorProcessingService.new(@color, current_user,).process_color(mapped_color, weather_data)
     @color = ColorForm.new(color_params, ai_comment: ai_comment)
     if @color.save
       redirect_to colors_path, notice: "色とAIレスポンスを保存しました"
@@ -32,18 +32,16 @@ class ColorsController < ApplicationController
   end
 
   def update
-
         mapped_color = ColorMapping.mapping_color(color_params[:color_name])
         weather_data = color_params.slice(:weather_name, :weather_pressure, :temperature)
-        ai_comment = ColorProcessingService.new(@color, current_user).process_color(mapped_color,weather_data)
+        ai_comment = ColorProcessingService.new(@color, current_user).process_color(mapped_color, weather_data)
         @color_form = ColorForm.new(color_params.merge(color: @color), ai_comment: ai_comment)
         if @color_form.save
         redirect_to colors_path(@color), notice: "色の更新に成功しました！"
-      else
+        else
         flash.now[:alert] = "編集失敗"
         render :edit, status: :unprocessable_entity
-      end
-    rescue ActiveRecord::RecordInvalid => e
+        end
       flash.now[:alert] = "更新に失敗しました"
       render :edit, status: :unprocessable_entity
   end
--- app/forms/color_form.rb
+++ app/forms/color_form.rb
@@ -2,18 +2,18 @@ class ColorForm
   include ActiveModel::Model
   include ActiveModel::Attributes
   # 親モデルの属性
-  attribute :color_name ,:integer
+  attribute :color_name, :integer
   # 子モデルの属性(中間)
-  attribute :color_id ,:integer
-  attribute :user_id ,:integer
+  attribute :color_id, :integer
+  attribute :user_id, :integer
   # 子モデルの属性(weather_log)
   attribute :city, :string
   attribute :weather_name, :string
   attribute :description, :string
-  attribute :temperature ,:integer
-  attribute :temp_max ,:integer
-  attribute :temp_min ,:integer
-  attribute :weather_pressure ,:integer
+  attribute :temperature, :integer
+  attribute :temp_max, :integer
+  attribute :temp_min, :integer
+  attribute :weather_pressure, :integer
   attribute :weather_icon, :string
   # 子モデルの属性(response)
   attribute :ai_comment, :string
@@ -45,7 +45,7 @@ class ColorForm
       self_log.responses.create!(ai_comment: @ai_comment)
 
       weather_log = self_log.weather_log || self_log.build_weather_log
-      weather_log.update!( city: city, weather_name: weather_name, description: description,
+      weather_log.update!(city: city, weather_name: weather_name, description: description,
       temperature: temperature, temp_max: temp_max, temp_min: temp_min,
       weather_pressure: weather_pressure, weather_icon: weather_icon)
     end
