4a07d1197dbf1d33b72e23f6aaab8a15a21d0f9e
inoueyuji <sikogaku2593@gmail.com>
2025-03-03 02:03:44 +0900

    custum

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  mount LetterOpenerWeb::Engine, at: "/letter_opener"
-
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
   devise_for :users, controllers: {
     sessions: "users/sessions",
     registrations: "users/registrations",
