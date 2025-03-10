9a4d9863598da553824671196dcbde90ef4b8d19
inoueyuji <sikogaku2593@gmail.com>
2025-03-10 20:02:50 +0900

    fix:custumPasswordReset

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -10,5 +10,5 @@ application.register("form-reset", FormResetController)
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
 
-import PasswordModalController from "./password_modal_controller"
-application.register("password-modal", PasswordModalController)
+import ModalController from ".modal_controller"
+application.register("modal", ModalController)
