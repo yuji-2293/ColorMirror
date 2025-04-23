f9326a084c9f389dcf68de5c9ea8d161a0fb7bf6
inoueyuji <sikogaku2593@gmail.com>
2025-04-24 05:13:10 +0900

    fix:radarChart

--- app/controllers/colors_controller.rb
+++ app/controllers/colors_controller.rb
@@ -21,7 +21,7 @@ class ColorsController < ApplicationController
     prompt_mood = params[:mood]
     weather_data = color_params.slice(:weather_name, :weather_pressure, :temperature)
     # AIレスポンス生成のserviceを呼び出す/必要な引数を渡す、生成したレスポンスを変数に格納
-    ai_responses= ColorProcessingService.new(@color, current_user).process_color(prompt_color, weather_data, prompt_mood)
+    ai_responses = ColorProcessingService.new(@color, current_user).process_color(prompt_color, weather_data, prompt_mood)
     @color.color_analysis = ai_responses[:color_analysis]
     @color.weather_analysis = ai_responses[:weather_analysis]
     if @color.save
